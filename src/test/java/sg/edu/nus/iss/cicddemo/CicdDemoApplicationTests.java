a775310871d2e9b638fc89184a1705f15f35ef3f
brittajaison <britta@nus.edu.sg>
2024-01-24 23:23:38 +0800

    Resolve code smell

--- src/test/java/sg/edu/nus/iss/cicddemo/CicdDemoApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/cicddemo/CicdDemoApplicationTests.java
@@ -1,17 +1,22 @@
 package sg.edu.nus.iss.cicddemo;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CicdDemoApplicationTests {
 
+	@Autowired
+    private CicdDemoApplication applicationContext;
+ 
 	@Test
-	void contextLoads(ApplicationContext context) {
-	       assertThat(context).isNotNull();
+	void contextLoads() {
+		assertNotNull(applicationContext, "Application context should not be null");
 	    }
 
 }
